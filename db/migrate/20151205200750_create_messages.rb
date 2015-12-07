2f9016ad5e8b543ecd5e04127feb84f7aca04a00
bklynate <nathanielcfa@gmail.com>
2015-12-06 20:01:18 -0500

    change user migration file

--- db/migrate/20151205200750_create_messages.rb
+++ db/migrate/20151205200750_create_messages.rb
@@ -3,7 +3,7 @@ class CreateMessages < ActiveRecord::Migration
     create_table :messages do |t|
       t.string :title, null: false
       t.text :content, null: false
-      t.integer :user_id, null: false
+      t.integer :user_id
 
       t.timestamps null: false
     end
