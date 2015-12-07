65b1c0bfbf6c9cc2eb43bea7b608a815ab06f504
bklynate <nathanielcfa@gmail.com>
2015-12-06 19:41:10 -0500

    add new.html.erb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,19 @@
 class MessagesController < ApplicationController
+  def new
+    @message = Message.new
+  end
+
   def create
     @message = Message.new(message_params)
+
+    if @message.save
+      flash[:success] = "Your message was created!"
+      redirect_to @message
+    else
+      render 'new'
+    end
   end
+
 
   private
 
