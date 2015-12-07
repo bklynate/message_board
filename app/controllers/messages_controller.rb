86c010ecc80577c3c0f96589eaeaccae189780d4
bklynate <nathanielcfa@gmail.com>
2015-12-06 22:58:54 -0500

    error

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -21,6 +21,6 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:title, :content,:user_id)
+    params.require(:message).permit(:title, :content)
   end
 end
