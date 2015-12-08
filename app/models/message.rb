caff7fedb30ded485b89b8aeacaf892536c1eea0
bklynate <nathanielcfa@gmail.com>
2015-12-07 19:10:08 -0500

    add completes a 'all messages page' railsbridge challenge

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
 
   validates :title, presence: true
   validates :content, presence: true
--- db/migrate/20151205200750_create_messages.rb
+++ db/migrate/20151205200750_create_messages.rb
@@ -4,7 +4,7 @@ class CreateMessages < ActiveRecord::Migration
       t.string :title, null: false
       t.text :content, null: false
       t.integer :user_id, index: true, foreign_key: true, null: false
-
+      
       t.timestamps null: false
     end
   end
